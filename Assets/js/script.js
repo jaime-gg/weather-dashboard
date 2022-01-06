c6ad23e7018c9fd3c61c9957cea77395466f7e95
jester <jaimebuddy98@gmail.com>
2022-01-06 15:30:15 -0600

    added some ids and simple js

---
+++ Assets/js/script.js
@@ -0,0 +1,8 @@
+
+// global variables 
+var APIKey = "&units=imperial&appid=e7111b1f6589775ae781984a6af9beb7";
+var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?"
+
+// form & history slection 
+var searchCityForm = $(#searchCityForm);
+var searchedCities = $(#searchedCities);
